ec4dca2805a2bdbb9688624c05f3346e81a6df60
scorpizey <scorpizey25@gmail.com>
2025-08-26 23:54:53 +0700

    update register_user.py v2 condition if..else

--- register_user.py
+++ register_user.py
@@ -7,3 +7,11 @@ print(fullname)
 print(username)
 print(password)
 print(tanggal)
+
+if password == "pwd_scorpizey":
+    if len(password) >= 5:
+        print("password match")
+    else:
+        print("password must containt more 2 length")
+else:
+    print("password incorrect")
