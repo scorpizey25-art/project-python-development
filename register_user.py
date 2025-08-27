9772c0f29f69f56d01196edb57576295ea3624df
scorpizey <scorpizey25@gmail.com>
2025-08-27 18:12:57 +0700

    perbaruan register_user.py conflic

--- register_user.py
+++ register_user.py
@@ -1,17 +1,34 @@
+import array;
+import sys;
+
 fullname = input("Masukkan Nama kamu :")
 username = input("Masukkan Username kamu :")
 password = input("Masukkan Password kamu :")
 tanggal = "01-11-1987"
 
-print(fullname)
-print(username)
-print(password)
-print(tanggal)
+arr_panjang_password = array.array('i',[1,2,3,4,5]);
 
-if password == "pwd_scorpizey":
-    if len(password) >= 5:
+#print(fullname)
+#print(username)
+#print(password)
+#print(tanggal)
+
+if fullname is None or fullname == '':
+    print("fullname tidak boleh kosong !!!")
+    sys.exit()
+else:
+    username = input("Masukkan Username kamu :")
+    password = input("Masukkan Password kamu :")
+    tanggal  = "01-11-1987"
+
+if password == "12":
+    if len(password) >= arr_panjang_password[4]:
         print("password match")
     else:
-        print("password must containt more 2 length")
+        #jika numeric dialam {} pakai => 
+        # simbol f"isi string {}", atau 
+        # gunakan format({}), atau 
+        # gunakan % {}
+        print(f"password must containt more {arr_panjang_password[4]} length")
 else:
     print("password incorrect")
