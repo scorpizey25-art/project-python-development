9700fe24c3dbe13e938697cf00a458c34ddd2c89
scorpizey25-art <scorpizey25@gmail.com>
2025-08-27 11:48:40 +0000

    perubahan pc kkkantorr

--- register_user.py
+++ register_user.py
@@ -1,20 +1,10 @@
-import array;
-import sys;
+import array
+import sys
 
 fullname = input("Masukkan Nama kamu :")
-<<<<<<< HEAD
-username = input("Masukkan Username kamu :")
-password = input("Masukkan Password kamu :")
-tanggal = "01-11-1987"
-=======
->>>>>>> 6dcfc8e (update terbaru register_user.py)
 
-arr_panjang_password = array.array('i',[1,2,3,4,5]);
+arr_panjang_password = array.array('i', [1, 2, 3, 4, 5]);
 
-#print(fullname)
-#print(username)
-#print(password)
-#print(tanggal)
 
 if fullname is None or fullname == '':
     print("fullname tidak boleh kosong !!!")
@@ -22,13 +12,13 @@ if fullname is None or fullname == '':
 else:
     username = input("Masukkan Username kamu :")
     password = input("Masukkan Password kamu :")
-    tanggal  = "01-11-1987"
+    tanggal = "01-11-1987"
 
 if password == "12":
     if len(password) >= arr_panjang_password[4]:
         print("password match")
     else:
-        #jika numeric dialam {} pakai => 
+        # jika numeric dialam {} pakai =>
         # simbol f"isi string {}", atau 
         # gunakan format({}), atau 
         # gunakan % {}
