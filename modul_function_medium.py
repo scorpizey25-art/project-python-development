cff78344a5fdae3f904e65aa106a9a1fd7857755
scorpizey25-art <scorpizey25@gmail.com>
2025-08-30 07:55:45 +0000

    push from kantor

--- modul_function_medium.py
+++ modul_function_medium.py
@@ -2,6 +2,30 @@
 # tanpa mendefinisikan nama fungsi
 # dan function disimpan pada variabel
 
-tambah = lambda a, b: a+b
+# secara aturan flake8
+# operasi ini sangat tidak direkomendasikan
+# karena mengurangi keterbacaan kode
+# lebih baik menggunakan def function
+# namun untuk tugas-tugas kecil
+# penggunaan lambda function masih diperbolehkan
+#   => tambah = lambda a, b: a+b
+#   => print(tambah(10, 10))
 
-print(tambah(10, 10))
+
+# function decorator
+def decorator_wrapper(fcontaint):    # membuat fungsi decorator
+    print("Membuat Pembungkus")
+
+    def create_wrapper():   # membuat fungsi pembungkus
+        print("Menambahkan Pita Kuning")
+        fcontaint()
+        print("Menambahkan Pita Pink")
+    return create_wrapper    # mengembalikan fungsi pembungkus
+
+@decorator_wrapper    # menambahkan decorator pada fungsi
+def IsiKado():   # fungsi yang akan dibungkus
+    print("Ini adalah kado spesial untukmu")
+
+
+# memanggil fungsi yang sudah dibungkus
+IsiKado()   # fungsi yang akan dibungkus
